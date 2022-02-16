bc2ac26a520f391c7cba8542dc9ddb323e10402a
montyhobratschk <98488368+montyhobratschk@users.noreply.github.com>
2022-02-16 13:15:55 -0600

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,6 +22,7 @@ namespace API
         public Startup(IConfiguration config)
         {
             _config = config;
+            
         }
 
         public IConfiguration Configuration { get; }
@@ -35,6 +36,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -54,7 +56,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
